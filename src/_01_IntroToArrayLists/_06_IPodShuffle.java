d5206b54f059bda611f20979d42e5f28cea485d7
1luckybiscuit <lukasnepo11@gmail.com>
2018-12-01 17:19:02 -0800

    iPodShuffle (IP)

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -2,23 +2,32 @@ package _01_IntroToArrayLists;
 
 import java.util.ArrayList;
 import java.util.Random;
-
-
-//Copyright The League of Amazing Programmers, 2015
+import javax.swing.*;
 
 public class _06_IPodShuffle{
 	public _06_IPodShuffle() {
-		// 1. Use the Song class the play the demo.mp3 file.
-				
-				
-		/**
-		 * 2. Congratulations on completing the sound check! * Now we want to make an
-		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
-		 * "Surprise Me!" button that will play a random song when it is clicked. * If
-		 * you're really cool, you can stop all the songs, before playing a new one on
-		 * subsequent button clicks.
-		 */
-		
+		ArrayList<Song> songs = new ArrayList<Song>();
+		Song s1 = new Song("cat.mp3");
+		Song s2 = new Song("drums.mp3");
+		Song s3 = new Song("reg.mp3");
+		Song s4 = new Song("demo.mp3");
+		Song s5 = new Song("inst.mp3");
+		Song s6 = new Song("cred.mp3");
+		Song s7 = new Song("wither.mp3");
+		Song s8 = new Song("train.mp3");
+		Song s9 = new Song("ocelot.mp3");
+		songs.add(s1);
+		songs.add(s2);
+		songs.add(s3);
+		songs.add(s4);
+		songs.add(s5);
+		songs.add(s6);
+		songs.add(s7);
+		songs.add(s8);
+		songs.add(s9);
+		JFrame f = new JFrame("iPod Shuffle");
+		JPanel p = new JPanel();
+		JButton surp = new JButton("Surprise Me With A Song!!!!!!!");
 	}
 	
 	public static void main(String[] args) {

dbb812d40978e0c8a2f02d080b979592a403450c
Samuel Sharp <League@AdministorsiMac.attlocal.net>
2019-01-05 17:24:57 -0800

    LogSearch, HangMan (IP), PlayHM (IP)
    Note to self: Add languages to main menu. Add background color. Add
    more junk…

--- src/_04_HangMan/PlayHM.java
+++ src/_04_HangMan/PlayHM.java
@@ -1,6 +1,8 @@
 package _04_HangMan;
 
 import java.awt.Cursor;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -8,33 +10,133 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class PlayHM {
-	Utilities util = new Utilities();
+public class PlayHM implements KeyListener {
 	Stack<String> words = new Stack<String>();
-	JFrame f = new JFrame("SAMUEL HANGMAN");
-	JLabel wd = new JLabel("WORD NOT LOADED ERROR: ERROR CODE 100");
-	JLabel lv = new JLabel("LIVES NOT LOADED: ERROR CODE 101");
+	JFrame f = new JFrame("HangMan 1.0 Alpha");
 	JPanel p = new JPanel();
+	JLabel l = new JLabel("Guess the word:");
+	JLabel li = new JLabel("Lives left: 0!!!!");
+	int lives = 0;
+	String word = Utilities.readRandomLineFromFile("dictionary.txt");
 
 	public PlayHM() {
-		f.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
-		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		f.setUndecorated(true);
-		String numwords = JOptionPane.showInputDialog(null, "Select a number of words!", "Welcome to S HangMan!",
-				JOptionPane.QUESTION_MESSAGE);
-		int numw = Integer.parseInt(numwords);
-		for (int i = 0; i < numw - 1; i++) {
-			String word = Utilities.readRandomLineFromFile("dictionary.txt");
+		f.addKeyListener(this);
+		p.add(l);
+		p.add(li);
+		f.add(p);
+		String nw = JOptionPane.showInputDialog(null,
+				"Type a number! Any number less than or equal to the number of words in dictionary.txt!",
+				"Word Select | Samuel HangMan", JOptionPane.PLAIN_MESSAGE);
+		int numwords = Integer.parseInt(nw);
+		for (int i = 0; i < numwords; i++) {
 			words.push(word);
 			System.out.println(word);
-			for (int j = 1; i < word.length(); j++) {
-				wd.setText(wd.getText() + "_");
-				f.setSize(1919, 1080);
-				f.setVisible(true);
-			}
-			p.add(wd);
-			p.add(lv);
-			f.add(p);
 		}
+		String w = words.pop();
+		for (int i = 0; i < w.length(); i++) {
+			String nt = l.getText() + "_";
+			l.setText(nt);
+			lives = 5;
+		}
+		f.pack();
+		f.setVisible(true);
+		li.setText("Lives left: " + lives + "!");
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("Key");
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			String matchst = "a";
+			StringBuilder chword = new StringBuilder(word);
+			char match = matchst.charAt(0);
+			chword.setCharAt(word.indexOf(matchst), match);
+		}
+		if (e.getKeyCode() == KeyEvent.VK_B) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_C) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_D) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_E) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_F) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_G) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_H) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_I) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_J) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_K) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_L) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_M) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_N) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_O) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_P) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_Q) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_R) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_S) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_T) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_U) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_V) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_X) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_Y) {
+
+		}
+		if (e.getKeyCode() == KeyEvent.VK_Z) {
+
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
 	}
 }
--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -14,17 +14,10 @@ public class _02_LogSearch implements ActionListener {
 	HashMap<Integer, String> log = new HashMap<Integer, String>();
 	JFrame f = new JFrame("Samuel ListManager");
 	JButton add = new JButton("Add Entry");
-	JButton search = new JButton("Search list by ID");
+	JButton search = new JButton("Lookup by ID");
 	JButton view = new JButton("View List");
+	JButton delete = new JButton("Delete Entry by ID");
 	JPanel p = new JPanel();
-
-	/*
-	 * When this is complete, add a fourth button to your window. Button 4: Remove
-	 * Entry When this button is clicked, prompt the user to enter an ID using an
-	 * input dialog. If this ID exists in the HashMap, remove it. Otherwise, notify
-	 * the user that the ID is not in the list.
-	 *
-	 */
 	public static void main(String[] args) {
 		_02_LogSearch ls = new _02_LogSearch();
 		ls.go();
@@ -34,11 +27,13 @@ public class _02_LogSearch implements ActionListener {
 		p.add(add);
 		p.add(search);
 		p.add(view);
+		p.add(delete);
 		add.addActionListener(this);
 		view.addActionListener(this);
 		search.addActionListener(this);
+		delete.addActionListener(this);
 		f.add(p);
-		f.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+		f.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		f.setUndecorated(true);
 		f.setVisible(true);
@@ -61,8 +56,19 @@ public class _02_LogSearch implements ActionListener {
 			JOptionPane.showMessageDialog(null, text, "View Log", JOptionPane.PLAIN_MESSAGE);
 		}
 		if (src == search) {
-			String id = JOptionPane.showInputDialog("ID:");
-			JOptionPane.showMessageDialog(null, "ID" + id + " is registered to " + log.get(id));
+			String ids = JOptionPane.showInputDialog("ID:");
+			int id = Integer.parseInt(ids);
+			JOptionPane.showMessageDialog(null, "ID " + id + " is registered to " + log.get(id));
 		}
+		if(src == delete) {
+			String ids = JOptionPane.showInputDialog("ID:");
+			int id = Integer.parseInt(ids);
+			if(log.get(id) == null) {
+			JOptionPane.showMessageDialog(null, "ERROR 101: User does not exist");
+			}
+			else {
+				log.remove(id);
+			}
+			}
 	}
 }

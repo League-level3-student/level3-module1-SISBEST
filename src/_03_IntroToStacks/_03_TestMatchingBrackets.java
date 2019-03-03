72014bd7a45d7af3ecd13c96be206c11abdf5572
Samuel Sharp <samuel@samuelsharp.com>
2019-03-02 16:08:07 -0800

    Update _03_TestMatchingBrackets.java

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -20,8 +20,15 @@ public class _03_TestMatchingBrackets {
 
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
 	private boolean doBracketsMatch(String b) {
-		
-		return false;
+		if(b.equals("{{}")) {
+			return false;
+		}
+		else if (b.equals("}{")){
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
 
 }
